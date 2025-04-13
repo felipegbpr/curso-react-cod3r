6cb9c461df934fedd2a1f23fba2b67d4cad0ec1c
Felipe Borges <felipetecacc@gmail.com>
2025-04-13 14:09:05 -0300

    Conhecendo JSX

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 
-const el = document.getElementById('root');
+const tag = <strong>Olá React</strong>;
 
-ReactDOM.render('Olá React', el);
+ReactDOM.render(<div>{tag}</div>, document.getElementById('root'));
