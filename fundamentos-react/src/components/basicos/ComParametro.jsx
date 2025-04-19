e44f2b92f4abcee07fec3ff68e6f961888fba5e2
Felipe Borges <felipetecacc@gmail.com>
2025-04-19 20:39:39 -0300

    Propriedades são Somente Leitura

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function ComParametro(props) {
     const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação';
+    const notaInt = Math.ceil(props.nota);
     return (
         <div>
             <h2>{ props.titulo }</h2>
             <p>
-                <strong>{ props.aluno }</strong> possui nota <strong>{ props.nota }</strong> e está <strong>{ status }!</strong>
+                <strong>{ props.aluno }</strong> possui nota <strong>{ notaInt }</strong> e está <strong>{ status }!</strong>
             </p>
         </div>
     );
