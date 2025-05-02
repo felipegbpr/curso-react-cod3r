bde2e3ed3f4e4db47298b047eb639a9a1d77bf5a
Felipe Borges <felipetecacc@gmail.com>
2025-05-02 19:23:58 -0300

    Componente com Estado

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default (props) => {
   const cb = props.quandoClicar;
+  const gerarIdade = () => parseInt(Math.random() * 20) + 50;
+  const gerarNerd = () => Math.random() > 0.5;
   return (
     <div>
       <div>Filho</div>
       <button
         onClick={(e) => {
-          cb('João', 64, true);
+          cb('João', gerarIdade(), gerarNerd());
         }}
       >
         Fornecer Infomarções
