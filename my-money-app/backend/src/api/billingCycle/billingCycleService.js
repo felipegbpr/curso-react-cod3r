0f63f604d1a232b4f6ceab6252fee8a3d16cf9bf
Felipe Borges <felipetecacc@gmail.com>
2025-08-07 23:08:37 -0300

    Middleware Query Parser Int

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -1,23 +1,32 @@
 const BillingCycle = require('./billingCycle');
 const errorHandler = require('../common/errorHandler');
 
+// Definição dos métodos e opções da rota em uma única chamada.
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
 BillingCycle.after('post', errorHandler).after('put', errorHandler);
 
+// Desabilita a paginação padrão do node-restful para que você possa controlá-la manualmente.
 BillingCycle.route('get', (req, res, next) => {
+    // Captura e converte os parâmetros de paginação da query, com valores padrão.
+    const skip = parseInt(req.query.skip) || 0;
+    const limit = parseInt(req.query.limit) || 10;
 
-    BillingCycle.find({}, (err, docs) => {
-        if(!err) {
-            res.json(docs);
-        } else {
-            res.status(500).json({errors: [err]});
-        }
-    });
+    // Executa a query com as opções de skip e limit.
+    BillingCycle.find({})
+        .skip(skip)
+        .limit(limit)
+        .exec((err, docs) => {
+            if (!err) {
+                res.json(docs);
+            } else {
+                res.status(500).json({errors: [err]});
+            }
+        });
 });
 
 BillingCycle.route('count', (req, res, next) => {
-    BillingCycle.count((err, value) => {
+    BillingCycle.countDocuments((err, value) => { // Use countDocuments() em Mongoose 5+
         if (err) {
             res.status(500).json({errors: [err]});
         } else {
