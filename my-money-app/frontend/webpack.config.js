f5ad558943be67f2f7ff4dec3ca1f6fd73f9b551
Felipe Borges <felipetecacc@gmail.com>
2025-09-10 17:58:14 -0300

    Componentes Index e App

--- my-money-app/frontend/webpack.config.js
+++ my-money-app/frontend/webpack.config.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
@@ -9,14 +9,14 @@ module.exports = {
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules:__dirname + '/node_modules',
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js' 
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
     plugins: [ 
