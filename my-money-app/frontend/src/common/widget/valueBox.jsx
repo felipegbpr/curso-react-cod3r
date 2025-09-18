e33f04773c4c22905ccfd9d4ffab53a9a8e81cf6
Felipe Borges <felipetecacc@gmail.com>
2025-09-17 22:36:33 -0300

    Componente ValueBox

---
+++ my-money-app/frontend/src/common/widget/valueBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Grid from '../layout/grid';
+
+export default props => (
+    <Grid cols={props.cols}>
+        <div className={`small-box bg-${props.color}`}>
+            <div className='inner'>
+                <h3>{props.value}</h3>
+                <p>{props.value}</p>
+            </div>
+        </div>
+        <div className='icon'>
+            <i className={`fa fa-${props.icon}`}></i>
+        </div>
+    </Grid>
+);
