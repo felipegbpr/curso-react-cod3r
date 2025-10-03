b94b8aa29700c7a82030ab671341699d4e7def1e
Felipe Borges <felipetecacc@gmail.com>
2025-10-02 22:20:38 -0300

    Componente CreditList Básico

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,6 +5,7 @@ import { reduxForm, Field } from 'redux-form';
 
 import { init } from './billingCycleActions';
 import labelAndInput from '../common/form/labelAndInput';
+import CreditList from './creditList';
 
 class BillingCycleForm extends Component {
 
@@ -39,6 +40,8 @@ class BillingCycleForm extends Component {
                         placeholder='Informe o ano'
                         readOnly={readOnly}
                     />
+                    <CreditList cols='12 6' />
+
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
