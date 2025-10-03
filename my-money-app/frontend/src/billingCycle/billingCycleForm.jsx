cdcbd863707d3830bd39eb28c94741861657170e
Felipe Borges <felipetecacc@gmail.com>
2025-10-03 19:59:29 -0300

    Adicionando o Campo Status no ItemList

--- my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,7 +10,7 @@ import ItemList from './itemList';
 class BillingCycleForm extends Component {
 
     render() {
-        const { handleSubmit, readOnly, credits } = this.props;
+        const { handleSubmit, readOnly, credits, debts } = this.props;
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -41,6 +41,7 @@ class BillingCycleForm extends Component {
                         readOnly={readOnly}
                     />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} field='credits' legend='Créditos' />
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly} field='debts' legend='Débitos' showStatus={true} />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -53,6 +54,9 @@ class BillingCycleForm extends Component {
 
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm);
 const selector = formValueSelector('billingCycleForm');
-const mapStateToProps = state => ({credits: selector(state, 'credits')}); 
+const mapStateToProps = state => ({ 
+    credits: selector(state, 'credits'),
+    debts: selector(state, 'debts')
+}); 
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm);
